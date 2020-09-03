7a6651f15d0f814973319faebc5c9352ee94457a
Benjamin Smith <benjamin.lee.smith@gmail.com>
2020-09-02 21:31:09 -0600

    styled header

--- lib/com/riversurfreport/androidclient/main.dart
+++ lib/com/riversurfreport/androidclient/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:river_surf_report_client/com/riversurfreport/androidclient/styles/green_terminal_colors.dart';
 import 'routes/recent_reports_route.dart';
 
 void main() => runApp(MyApp());
@@ -14,7 +15,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 //        debugShowCheckedModeBanner: false,
-        home: RecentReportsRoute(apiHomeUrl)
+        home: RecentReportsRoute(apiHomeUrl),
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(0, 0, 0, 1.0),
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+              title: TextStyle(
+                color: GreenTerminalColors.greenTextColor,
+                fontSize: 20
+              )
+            ),
+            iconTheme: IconThemeData(
+              color: GreenTerminalColors.greenTextColor
+            )
+          )
+        ),
+
     );
   }
 }
