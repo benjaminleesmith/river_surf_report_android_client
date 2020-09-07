33850f75686458d5ae64c0416464fd5ce7b77eba
Benjamin Smith <benjamin.lee.smith@gmail.com>
2020-09-06 22:16:33 -0600

    set icons

--- lib/com/riversurfreport/androidclient/main.dart
+++ lib/com/riversurfreport/androidclient/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-//        debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: RecentReportsRoute(apiHomeUrl),
         theme: ThemeData(
           fontFamily: 'VT323',
