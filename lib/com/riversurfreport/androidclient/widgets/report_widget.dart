e3473f8cb50ba440accc568a294efe7cd4f19929
Benjamin Smith <benjamin.lee.smith@gmail.com>
2020-09-21 23:10:35 -0600

    tab on image to show overlay

--- lib/com/riversurfreport/androidclient/widgets/report_widget.dart
+++ lib/com/riversurfreport/androidclient/widgets/report_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:river_surf_report_client/com/riversurfreport/androidclient/routes/wave_route.dart';
@@ -5,8 +7,8 @@ import 'package:river_surf_report_client/com/riversurfreport/androidclient/style
 import 'package:river_surf_report_client/com/riversurfreport/api/models/report.dart';
 import 'package:intl/intl.dart';
 
-class ReportWidget extends StatelessWidget {
-  const ReportWidget({
+class ReportWidget extends StatefulWidget {
+  const  ReportWidget({
     Key key,
     @required this.report,
     @required this.context,
@@ -25,22 +27,61 @@ class ReportWidget extends StatelessWidget {
       fontSize: 20, height: 2, color: GreenTerminalColors.greenTextColor);
 
   @override
+  _ReportWidgetState createState() => _ReportWidgetState();
+}
+
+class _ReportWidgetState extends State<ReportWidget> {
+  bool showDetails = false;
+
+  @override
   Widget build(BuildContext context) {
     Widget waveName;
 
-    if(waveLink) {
-      waveName = new GestureDetector(onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => WaveRoute(report.wave))
-        );
-      },
-          child: Text(report.wave.name, style: waveNameStyle.apply(decoration: TextDecoration.underline))
+    if(widget.waveLink) {
+      waveName = GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => WaveRoute(widget.report.wave))
+          );
+        },
+        child: Text(widget.report.wave.name, style: ReportWidget.waveNameStyle.apply(decoration: TextDecoration.underline))
       );
     } else {
-      waveName = Text(DateFormat('MMM d, yyyy').format(report.date), style: waveNameStyle);
+      waveName = Text(DateFormat('MMM d, yyyy').format(widget.report.date), style: ReportWidget.waveNameStyle);
     }
 
+    List<Widget> imageStack = <Widget>[];
+
+    imageStack.add(
+      Image.network(
+        widget.report.imageUrl,
+        fit: BoxFit.fitWidth,
+        width: widget.width,
+        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
+          if (loadingProgress == null)
+            return child;
+          return Center(
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes
+                : null,
+            ),
+          );
+        }
+      )
+    );
+
+    if(showDetails) {
+      imageStack.add(
+        Container(
+          color: Colors.black.withOpacity(0.8),
+          width: widget.width,
+          height: 100,
+          child: Text("foo")
+        )
+      );
+    }
 
     return Column(
       children: <Widget>[
@@ -51,20 +92,19 @@ class ReportWidget extends StatelessWidget {
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
                   waveName,
-                  Text(report.flow, style: flowStyle)
+                  Text(widget.report.flow, style: ReportWidget.flowStyle)
                 ])
         ),
-        Image.network(report.imageUrl, fit: BoxFit.fitWidth, width: width, loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
-          if (loadingProgress == null)
-            return child;
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                  ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes
-                  : null,
-            ),
-          );
-        }),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              showDetails = !showDetails;
+            });
+          },
+          child: Stack(
+            children: imageStack,
+          ),
+        ),
         Row(
 
         )
